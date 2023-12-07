b15b21f6b6590eb27d976122467335a0d8362010
Alexandra Nario <alexandranario@gmail.com>
2023-12-06 17:48:13 -0800

    modified files

--- script.js
+++ script.js
@@ -10,15 +10,33 @@ function addStudentToClass(student){
         }, randomDelay);
     })
 }
+async function registerStudent(student) {
+    try {
+        const result = await addStudentToClass(student);
+        console.log(`registerStudent successful and the result is ${result} `);
+    } catch (error) {
+        console.error(`registerStudent failed with the error: ${error}`);
+
+    } 
+}
+
+// This code should sit at the bottom of your solution
+// Plan accordingly
 
 const students = [
+
     { name: "Alice", age: 20 },
     { name: "Bob", age: 17 },
     { name: "Charlie", age: 19 },
+
 ];
 
+
 for (const student of students) {
-    console.log("Next student...")
+    console.log("Next student...");
     registerStudent(student);
-}
+
+};
+
 //how to create a random number with a fixed range
+
